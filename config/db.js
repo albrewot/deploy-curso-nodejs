1c0ca6a0ff7f876a70027031548f67175aae2da2
Victor E Gonzalez P <vemiliogp@gmail.com>
2019-07-18 15:07:38 -0400

    [variables de entorno]

--- config/db.js
+++ config/db.js
@@ -3,10 +3,18 @@ require("dotenv").config();
 
 // console.log(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/test`);
 // mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/test`, {
-mongoose.connect(`mongodb://${process.env.DB_HOST}/leccion`, {
-  useCreateIndex: true,
-  useNewUrlParser: true
-});
+// mongoose.connect(`mongodb://${process.env.DB_HOST}/leccion`, {
+if (process.env.NODE_ENV === "production") {
+  mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/test?retryWrites=true&w=majority`, {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  });
+} else {
+  mongoose.connect(`mongodb://${process.env.DB_HOST}/leccion`, {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  });
+}
 
 mongoose.Promise = global.Promise;
 
